11dfc3e887bc8ca1904412d52c8d8f580d9732b1
Nathália de Deus <nathaliamassis@gmail.com>
2024-07-02 18:36:26 -0300

    person details star

--- src/screens/details/details.screen.tsx
+++ src/screens/details/details.screen.tsx
@@ -1,21 +1,26 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { Container, Title } from "./details.styles";
+import { Container, Header, Title } from "./details.styles";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { RootTabParamList } from "../../types";
 import { IPeople } from "../../interfaces/IPeople";
 import { getPeopleById } from "../../services/people/people.service";
-import { View } from "react-native";
 import { IFilm } from "../../interfaces/IFilm";
+import { FontAwesome6 as Icon } from "@expo/vector-icons";
 import { getFilmById } from "../../services/film/film.service";
 import Tabs from "./tabs/tabs.component";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = BottomTabScreenProps<RootTabParamList, "Details">;
 
 const Details: React.FC<Props> = ({ route }) => {
   const { url } = route.params;
-
+  const { goBack } = useNavigation();
   const [character, setCharacter] = useState<IPeople>();
   const [films, setFilms] = useState<IFilm[]>([]);
+  const [isStarred, setIsStarred] = useState<boolean>(false);
+
+  const handleOnPressStar = () => setIsStarred(!isStarred);
 
   const fetchPerson = useCallback(async () => {
     const id = url.split("/people/")[1];
@@ -45,9 +50,17 @@ const Details: React.FC<Props> = ({ route }) => {
 
   return (
     <Container>
-      <View>
+      <Header>
+        <TouchableOpacity data-testid="teste" onPress={goBack}>
+          <Icon name="chevron-left" size={24} color="#000000" />
+        </TouchableOpacity>
+
         <Title>{character?.name}</Title>
-      </View>
+
+        <TouchableOpacity onPress={handleOnPressStar}>
+          <Icon name="star" size={24} color="#000000" solid={isStarred} />
+        </TouchableOpacity>
+      </Header>
       <Tabs films={films} character={character as IPeople} />
     </Container>
   );
--- src/screens/details/details.styles.ts
+++ src/screens/details/details.styles.ts
@@ -2,15 +2,23 @@ import { StatusBar } from "react-native";
 import styled from "styled-components/native";
 
 const Container = styled.SafeAreaView`
-  margin-top: ${StatusBar.currentHeight ? StatusBar.currentHeight + "px" : 0};
+  margin-top: ${StatusBar.currentHeight
+    ? StatusBar.currentHeight + 16 + "px"
+    : 0};
   padding: 8px 24px 24px;
   height: 100%;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
 `;
 
 const Title = styled.Text`
   font-size: 32px;
   font-weight: bold;
-  margin-bottom: 16px;
   align-self: center;
 `;
 
